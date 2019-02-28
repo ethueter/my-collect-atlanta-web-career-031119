2f0b94e83438d21f5356dd0ea68b08905b8ee5f5
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-28 22:24:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,13 @@
 def my_collect(array)
   if block_given?
    x = 0 
-  while x < array.length do
-    new_array = []
-    array.each do |name|
-    new_array.push(name)
-  x +=1
-end 
+   while x < array.length do
+     new_array = []
+      array.each do |name|
+      new_array.push(name)
+      x +=1
+      end 
+   end
+  end
+  new_array
 end
-new_array 
-end
-
