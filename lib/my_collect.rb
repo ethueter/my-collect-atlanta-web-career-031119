2b490b9fcd04c4718556bd279fa3b5db9e3815e8
Tyler Hueter <tylerhueter08@gmail.com>
2019-02-28 22:23:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,8 @@ def my_collect(array)
    x = 0 
   while x < array.length do
     new_array = []
-    var = yield(array[x])
-    new_array.push(var)
+    array.each do |name|
+    new_array.push(name)
   x +=1
 end 
 end
